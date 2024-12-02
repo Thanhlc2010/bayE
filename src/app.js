bb0c2dec2e149de8f88c06233c3c7f8b02cefcfd
CamCam <themast3t@gmail.com>
2024-12-02 23:38:08 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,26 +7,23 @@ import addFavour from './favourites/favouritesAddController.js';
 import delFavour from './favourites/favouriteDelController.js';
 import { bigIntMiddleware } from './shared/middleware/bigIntMiddleware.js';
 
-console.log(process.env.DATABASE_URL);
-console.log("hihi");
+console.log(process.env.DATABASE_URL)
 const app = express();
 app.use(express.json());
 app.use(bigIntMiddleware);
-// app.use(dotenv);
-// app.use(cors);
+app.use(cors())
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
-// Gắn route account vào /api/account
 app.use('/api/account', accountRoutes);
-
 app.use('/api/favour', addFavour);
 app.use('/api/favour', delFavour);
 
-console.log(process.env.PORT);
+app.use('/api/', sellingRoutes);
 
 
 // dataMake()
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-// console.log(process.env.PORT);
