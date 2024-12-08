e035433d2d141a198a9de4eb3c2ce691a8390589
ppppHHHuuuu <doanvuquangphu2112@gmail.com>
2024-12-08 23:15:14 +0700

    Push Test Server Code

--- src/app.js
+++ src/app.js
@@ -32,7 +32,9 @@ app.use('/api/seller', sellingRoutes);
 app.use('/api', searchByKeywordRoutes);
 
 app.use('/api', imageRoutes);
-
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "src/dist", "index.html"));
+});
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
